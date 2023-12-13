f0665182f87d7260e3a55da339492397419a9c19
neuman <owiroadams@gmail.com>
2023-12-13 13:31:56 +0300

    completed

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide() {
+    let divide = (2000/100);
+    return divide
+}
+divide();
+
+const square = (a) => {
+    let calc = (a*a)
+    return calc
+}
+square(6);
+
+const add = (a,b) => {
+    let calc = (a+b)
+    return calc
+}
+square(6,6)
